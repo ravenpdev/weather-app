8ed371f0b3eeaee778e4aec5c5b0b996f9176bb3
RavenP <ravenp.dev@gmail.com>
2023-03-05 15:06:39 +0800

    restrict Button component props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,92 @@
-type ButtonProps = {
-  type: 'submit' | 'button'
-} & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type'>
+import { BorderRadius, Kind, Sizes } from '@/types'
 
-const Button = ({ type, children, ...restProps }: ButtonProps) => {
+type ButtonType = 'submit' | 'button' | 'reset'
+
+type ButtonProps = {
+  type: ButtonType
+  kind?: Kind
+  size?: Sizes
+  children: React.ReactNode
+  onClick?: () => void
+  border?: boolean
+  borderRadius?: BorderRadius
+}
+
+const Button = ({
+  type,
+  children,
+  onClick,
+  kind,
+  size,
+  border,
+  borderRadius,
+}: ButtonProps) => {
+  const classes = ['py-1 px-3']
+
+  if (border) {
+    classes.push('border')
+  }
+
+  switch (kind) {
+    case 'primary':
+      classes.push('bg-purple-500 text-purple-50 hover:bg-purple-600')
+      break
+    case 'secondary':
+      classes.push('bg-pink-500 text-pink-50 hover:bg-pink-600')
+      break
+    case 'warning':
+      classes.push('bg-amber-500 text-amber-50 hover:bg-amber-600')
+      break
+    case 'info':
+      classes.push('bg-sky-500 text-sky-50 hover:bg-sky-600')
+      break
+    case 'success':
+      classes.push('bg-teal-500 text-teal-50 hover:bg-teal-600')
+      break
+    case 'error':
+      classes.push('bg-red-500 text-red-50 hover:bg-red-600')
+      break
+    default:
+      classes.push('bg-gray-100 hover:bg-gray-200')
+  }
+
+  switch (size) {
+    case 'xs':
+      classes.push('text-xs')
+      break
+    case 'sm':
+      classes.push('text-sm')
+      break
+    case 'large':
+      classes.push('text-lg')
+      break
+  }
+
+  if (borderRadius) {
+    switch (borderRadius) {
+      case 'sm':
+        classes.push('rounded-sm')
+        break
+      case 'md':
+        classes.push('rounded-md')
+        break
+      case 'lg':
+        classes.push('rounded-lg')
+        break
+      case 'full':
+        classes.push('rounded-full')
+        break
+      default:
+        classes.push('rounded-none')
+    }
+  }
+
   return (
-    <button type={type} {...restProps}>
+    <button
+      className={classes.join(' ')}
+      type={type}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
