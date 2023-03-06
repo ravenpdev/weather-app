055f472cd583373db98cbf34c37cce7454814795
RavenP <ravenp.dev@gmail.com>
2023-03-06 12:19:56 +0800

    use the forecast hook

--- src/containers/WeatherSearchContainer/WeatherSearchContainer.tsx
+++ src/containers/WeatherSearchContainer/WeatherSearchContainer.tsx
@@ -1,70 +1,16 @@
-import { useEffect, useRef, useState } from 'react'
 import { WeatherSearch } from '@/components'
-import { Place } from '@/types'
-
-const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY
+import useForecast from '@/hooks/useForecast'
 
 const WeatherSearchContainer = () => {
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-  // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const [term, setTerm] = useState<string>('')
-  const [options, setOptions] = useState<Place[]>([])
-  const [selectedOption, setSelectedOption] = useState<Place | null>(null)
-
-  useEffect(() => {
-    inputFocus()
-  }, [])
-
-  const inputFocus = () => {
-    if (inputRef.current) {
-      inputRef.current.focus()
-    }
-  }
-
-  const getSearchOptions = (value: string) => {
-    if (value.trim() === '') {
-      setOptions([])
-      return
-    }
-
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data))
-  }
-
-  const handleOnInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-
-    setTerm(value)
-
-    getSearchOptions(value)
-  }
-
-  const getForecast = (value: Place) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${value.lat}&units=metric&lon=${value.lon}&appid=${API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }
-
-  const handleOnSubmit = () => {
-    if (!selectedOption) return
-
-    getForecast(selectedOption)
-
-    inputFocus()
-  }
-
-  const handleOnSelectOption = (value: Place) => {
-    setSelectedOption(value)
-    setTerm(value.name)
-    setOptions([])
-  }
+  const {
+    inputRef,
+    term,
+    options,
+    forecast,
+    handleOnInputChange,
+    handleOnSelectOption,
+    handleOnSubmit,
+  } = useForecast()
 
   return (
     <div className="-mt-20 w-full max-w-lg bg-white bg-opacity-20 py-24 px-16">
@@ -76,14 +22,21 @@ const WeatherSearchContainer = () => {
         dropdown
       </p>
 
-      <WeatherSearch
-        term={term}
-        options={options}
-        handleOnInputChange={handleOnInputChange}
-        handleOnSubmit={handleOnSubmit}
-        handleOnSelectOption={handleOnSelectOption}
-        inputRef={inputRef}
-      />
+      {forecast ? (
+        <section className="mt-5">
+          <h2>Forecast Result</h2>
+          {JSON.stringify(forecast)}
+        </section>
+      ) : (
+        <WeatherSearch
+          term={term}
+          options={options}
+          handleOnInputChange={handleOnInputChange}
+          handleOnSubmit={handleOnSubmit}
+          handleOnSelectOption={handleOnSelectOption}
+          inputRef={inputRef}
+        />
+      )}
     </div>
   )
 }
